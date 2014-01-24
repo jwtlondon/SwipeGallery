393ca1f3ef7de12c693707279d39904f9b659af8
bgarraud <bengarraud@gmail.com>
2014-01-24 13:51:43 +0000

    Add vimeo and youtube support

--- src/js/swipeGallery.js
+++ src/js/swipeGallery.js
@@ -34,8 +34,100 @@
             };
 
           
-         
+         var uniqueId = (function() {
+            var id = 0;
+            return function() {
+                return id++;
+            };
+        })();
 
+        function getQuerystring(url, key, default_) {
+            if (default_==null) default_="";
+            key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+            var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
+            var qs = regex.exec(url);
+            if(qs == null)
+            return default_;
+            else
+            return qs[1];
+        }
+
+
+
+        /**
+          * debouncedresize
+          **/
+        function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100)};return c};
+
+        /**
+          * Get youtube api
+          **/
+        function getVimeoApi() {
+            
+            if (typeof($f) == 'undefined' && window.loadingVimeoAPI==undefined) {
+              window.loadingVimeoAPI = true;
+              $.getScript('http://a.vimeocdn.com/js/froogaloop2.min.js').done(vimeoApiReady);
+            } 
+
+        }
+
+        /**
+          * initalise all vimeo players
+          **/
+        function vimeoApiReady() {
+            window.loadingVimeoAPI = undefined;
+            $('iframe').each(function(index,element) {
+                var iframe, player;
+
+                iframe = $(element);
+                if (iframe.attr('data-type')==='vimeo') {
+                    
+                     $f(element).addEvent('ready', function(player_id) {
+
+                        player = $f(element);
+                        iframe.parent().data('player',player);
+                     });
+                    
+                }
+
+            })
+
+        }
+        /**
+          * Get youtube api
+          **/
+        function getYoutubeApi() {
+            
+            if ((typeof(YT) == 'undefined' || typeof(YT.Player) == 'undefined') && window.loadingYTAPI==undefined) {
+             
+              window.loadingYTAPI = true;
+              window.onYouTubeIframeAPIReady = ytApiReady;
+              $.getScript('https://www.youtube.com/iframe_api');
+
+              
+
+            } 
+
+        }
+
+        /**
+          * initalise all youtube players
+          **/
+        function ytApiReady() {
+          window.loadingYTAPI = undefined;
+           
+            $('iframe').each(function(index,element) {
+                var iframe, player;
+
+                iframe = $(element);
+                if (iframe.attr('data-type')==='youtube') {
+                    player = new YT.Player(iframe.attr('id'));
+                   
+                    iframe.parent().data('player',player);
+                }
+
+            })
+        }
 
 
         /**
@@ -82,6 +174,10 @@
             this.initSwipe();
             this.buildContols();
             this.initControls();
+            this.initVideos();
+            
+
+            this.initResize();
 
 
             if (this.options.autoplay === true) {
@@ -120,6 +216,58 @@
         }   
 
 
+
+
+
+        /**
+         * Initalise resizecheck
+         */
+        SwipeGallery.fn.initResize = function() {
+            var component = this;
+              
+            var videoProxys = $('.video-proxy');
+
+           
+            if (videoProxys.length>0) {
+              on_resize(function() {
+                component.onResize(videoProxys);
+                 
+                  
+              });
+
+              component.onResize(videoProxys)
+            }
+
+            
+            if (this.options.paginator === true) {
+                this.pageButtons = this.$e.find('.pager');
+                this.pageButtons.on('click',function (event) {
+                    event.preventDefault();
+                    component.changeSlide(event.target)
+                    return false;
+                }).eq(0).addClass('active');
+            }
+
+
+        }
+
+
+        /**
+          * Uodate video proxies on resize
+          **/
+        SwipeGallery.fn.onResize = function(videoProxys) {
+            videoProxys.each(function(index, proxy) {
+                var container = $(proxy).parent();
+                if (container.hasClass('youtube-slide')) {
+                  $(proxy).css('height',container.height()-45);
+                }
+                if (container.hasClass('vimeo-slide')) {
+                  $(proxy).css('height',container.height()-51);
+                }
+            });
+
+
+        }
 
 
         /**
@@ -167,13 +315,27 @@
          * Initalise swipe plugin
          */
         SwipeGallery.fn.initSwipe = function() {
-            var component = this;
+            var component = this, player;
             this.options.swipeOptions.callback = function (index, elem) {
                 $(this.slides).removeClass('current').eq(index).addClass('current');
 
                 component.pageButtons.removeClass('active').eq(index).addClass('active');
 
                 component.options.callback && component.options.callback.call(this,index, elem);
+
+                // pause youtube player if it exsits
+                if ($(elem).hasClass('youtube-slide')) {
+                    player = $(elem).data('player');
+                    if (player.getPlayerState()==1 || player.getPlayerState()==3) {
+                        player.pauseVideo();
+                    }
+                }
+                if ($(elem).hasClass('vimeo-slide')) {
+                    player = $(elem).data('player');
+                    
+                    player.api('pause');
+                    
+                }
             };
             this.options.swipeOptions.transitionEnd = function (index, elem) {
                 component.options.transitionEnd && component.options.transitionEnd.call(this,index, elem);
@@ -185,6 +347,108 @@
             $(this.slides).eq(0).addClass('current');
         }
 
+
+        /**
+         * 
+         */
+        SwipeGallery.fn.initVideos = function() {
+            var component = this, video;
+            $(this.slides).each(function (listIndex, listItem) {
+                 video = $(this).find('iframe');     
+
+                 if (video.length>0) {
+                    component.parseVideo(video, $(this),listIndex);
+                 }
+            });
+
+
+
+
+        }
+
+         SwipeGallery.fn.parseVideo = function(video, container,listIndex) {
+            var url = video.attr('src');
+            var component = this;
+
+            
+            if (url.indexOf("youtube.com")>-1) {
+
+              getYoutubeApi();
+              video.attr('data-type','youtube')
+                   .attr('id','video'+listIndex);
+              container
+                  .addClass('resp-frame')
+                  .addClass('youtube-slide')
+                  .append('<div class="video-proxy"></div>');
+
+              container.find('.video-proxy')        
+                  .data('video', video.get(0))
+                  .on('click',function(event) { 
+                                      component.toggleVideo(event,'youtube') 
+                              });    
+
+            } else if (url.indexOf("vimeo.com")>-1) {
+              var viddeoID = getQuerystring(url, 'player_id','video'+listIndex);
+              
+
+              getVimeoApi();
+
+              video.attr('data-type','vimeo')
+                   .attr('id',viddeoID);
+              container
+                  .addClass('resp-frame')
+                  .addClass('vimeo-slide')
+                  .append('<div class="video-proxy"></div>');
+
+              container.find('.video-proxy')
+                  .data('video', video.get(0))
+                  .on('click',function(event) { 
+                                      component.toggleVideo(event,'vimeo') 
+                              });    
+
+            }
+        }
+
+        SwipeGallery.fn.toggleVideo = function(event,type) {
+            var el = $(event.target);
+            var video = el.data('video');
+            var player = $(video).parent().data('player');
+            
+            if (type == 'youtube') {
+              /**
+                * -1 (unstarted)
+                *  0 (ended)
+                *  1 (playing)
+                *  2 (paused)
+                *  3 (buffering)
+                *  5 (video cued).
+                **/
+
+              if (player.getPlayerState()==1 || player.getPlayerState()==3) {
+
+                  player.pauseVideo();
+              } else {
+                  player.playVideo();
+              }
+            }
+
+            if (type == 'vimeo') {
+              console.log('toggle b')
+              //player.api('play');
+              player.api('paused', function (value, player_id) {
+                         
+                  if (value==true) {
+                      player.api('play');
+
+                  } else {
+                      player.api('pause');
+
+                  }
+              });
+            }
+
+           
+        }
 
         /**
          * Change slide next / previous
@@ -238,6 +502,18 @@
              this.carousel.next();
              this.setTimer();
         }
+
+        SwipeGallery.fn.fixIframeVideos = function() {
+             var component = this;
+            $(this.slides).find('iframe').each(function (listIndex, listItem) {
+                        
+
+            });
+
+             //video-proxy
+        }
+
+
 
         /**
          * Create as jquery / Zepto plugin
